55c8ee1ab3be1c50492468b74e28012d1050016f
Paul Finegan <paul.s.finegan@gmail.com>
2020-05-30 12:48:02 +1000

    modified to handle empty input

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ app.route("/").get((req,res)=>{
 //create a route for api.  Accept the date string as a parameter.
 app.route("/api/timestamp/:date_string?").get((req,res)=>{
 	//test if the input is a number or not
-	const testDate = isNaN(req.params.date_string) ? new Date(req.params.date_string) : new Date(parseInt(req.params.date_string));
+	const testDate = req.params.date_string ? isNaN(req.params.date_string) ? new Date(req.params.date_string) : new Date(parseInt(req.params.date_string)) : new Date();
 	//if the date value which has been input is not a falsy, then send back the format required, otherwise send back an error.
 	testDate.valueOf() ? res.json({"unix":testDate.getTime(),"utc":testDate.toUTCString()}) : res.json({"error":"Invalid Date"})
 })
 
-app.listen(process.env.PORT()=>console.log("Timestamp service is listening....."))
+app.listen(process.env.PORT,()=>console.log("Timestamp service is listening....."))
 
