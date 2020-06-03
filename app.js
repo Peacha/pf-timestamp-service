9564830f86891f83dad38cc1a9587dca00e100e4
Peacha <paul.s.finegan@gmail.com>
2020-06-03 17:08:37 +1000

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.route("/").get((req,res)=>{
-	res.send("hello world");
-})
-
-app.listen(8080,()=>console.log("Timestamp service is listening....."))
